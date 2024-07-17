389b1bff846602f914dd9e7b3f16209da6e0dd7e
Anastas <ansts@yahoo.com>
2024-07-17 08:58:51 +0300

    making sure

--- plotG3D.R
+++ plotG3D.R
@@ -1,16 +1,20 @@
-plotG3D=function(G, att="C", atfl=T, e_att=NULL, extatt=NULL, omit=NULL, 
-                 coff=NULL, seed=777777, normaLap=T,smoothcol=T, interact=T){
+plotG3D=function(G, att="C", atfl=T, e_att=NULL, extatt=NULL, omit=NULL, grclr=F,
+                 coff=NULL, seed=777777, normaLap=T,smoothcol=T, interact=F){
   require(igraph)
   require(rgl)
   require(uwot)
   require(pbapply)
   
+  grclrs=colorRampPalette(c("black", gray(0.2,0), gray(0.6,0), gray(0.8,0),"white"))
   cpl=colorRampPalette(c("#000000","#0050AA9F","#10AA109F","#FFFF009F","#FFA0009F","#B50000"), alpha=T)
+  
+  if (grclr) clrplt=grclrs else clrplt=cpl
+  
   cpl4=c("#B5000090","#80808090","#10AA1090","#0020FF90")
   cm=components(G)
-  G=induced.subgraph(G, V(G)[cm$membership==as.numeric(names(table(cm$membership)))[which.max(table(cm$membership))]])
+  G=induced_subgraph(G, V(G)[cm$membership==as.numeric(names(table(cm$membership)))[which.max(table(cm$membership))]])
   n=vcount(G)
-  if (atfl) Att=vertex.attributes(G)[[att]] else Att=att[names(V(G))]
+  if (atfl) Att=vertex.attributes(G)[[att]] else Att=att
   if (normaLap) L=embed_laplacian_matrix(G,no=n-1, type = "I-DAD") else L=embed_laplacian_matrix(G,no=n-1)
   if( interact) {
     plot((n-50) : (n-1),L$D[(n-50) : (n-1)], xlab="# eigenvalue", ylab="The smallest non-zero eigenvalues")
@@ -26,14 +30,14 @@ plotG3D=function(G, att="C", atfl=T, e_att=NULL, extatt=NULL, omit=NULL,
   print(j)
   Mx=L$X[,j]
   
-  uMx=umap(Mx, n_neighbors = 50, min_dist = 0.005, spread=10,  n_epochs = 1500,
+  uMx=umap(Mx, n_neighbors = min(50,n), min_dist = 0.4, spread=7,  n_epochs = 1500,
            verbose=F, n_components = 3, init = "normlaplacian",n_sgd_threads=1, seed=seed)
   
   #uMx=cmdscale(dist(Mx), k=7, add=T, list. = T, eig=T)
   # GOF=uMx$GOF
   # uMx=uMx$points
 
-  if (smoothcol) vclr=cpl(vcount(G))[cut(Att, vcount(G), labels=F)] else vclr=Att
+  if (smoothcol) vclr=clrplt(vcount(G))[cut(Att, vcount(G), labels=F)] else vclr=Att
   names(vclr)=names(V(G))
   ecl=rep(1,ecount(G))
   if (!is.null(extatt)) eclr=extatt else {
@@ -44,13 +48,13 @@ plotG3D=function(G, att="C", atfl=T, e_att=NULL, extatt=NULL, omit=NULL,
         ecl=(edge_attr(G)[[e_att]]>4)*edge_attr(G)[[e_att]]
         eclr=ecl
         eclr[ecl==0|ecl>20]="#FFFFFF00"
-        eclr[ecl>0&ecl<20]=cpl(20)[ecl]
+        eclr[ecl>0&ecl<20]=clrplt(20)[ecl]
       }
   }
     
-    #eclr=cpl(ecount(G))[cut(edge_attr(G)[[e_att]], ecount(G), labels=F)]
- rglplot(G, layout=uMx, vertex.size=3, vertex.color=vclr, edge.color=eclr,
-         edge.width=0.2*(ecl>0), vertex.label=NA, vertex.frame.color=NA)
+    #eclr=clrplt(ecount(G))[cut(edge_attr(G)[[e_att]], ecount(G), labels=F)]
+ rglplot(G, layout=uMx, vertex.size=sqrt(vertex_attr(G)$size), vertex.color=vclr, edge.color=eclr,
+         edge.width=0.1*(ecl>0), vertex.label=NA, vertex.frame.color=NA)
  if (interact) readline("Adjust the window before hitting enter.")
 
   # pairs(Mx, col=vclr, pch=16, cex=0.5)
